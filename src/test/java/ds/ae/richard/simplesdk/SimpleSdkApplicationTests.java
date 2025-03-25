4d643139dd68ad1b9e37a3cda4da2351a114459f
继圣 <jisheng.yh@alibaba-inc.com>
2025-03-25 10:37:55 +0800

    Remove testing

--- src/test/java/ds/ae/richard/simplesdk/SimpleSdkApplicationTests.java
+++ src/test/java/ds/ae/richard/simplesdk/SimpleSdkApplicationTests.java
@@ -1,18 +1,13 @@
 package ds.ae.richard.simplesdk;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SimpleSdkApplication.class)
 class SimpleSdkApplicationTests {
-    public class Main {
-      //  public static void main(String[] args) {
-            // Example: Query product details
-           // String result = client.getProductDetails("yourAccessToken", "productId", "shipToCountry", "targetCurrency", "targetLanguage", false);
-          //  System.out.println(result);
-      //  }
-    }
+
     @Test
     void contextLoads() {
+        // 测试上下文加载
     }
-
 }
